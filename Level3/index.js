3a5cc8444d8e2ca94b706cecb99f7e6680c19d76
20wh1a0561 <20wh1a0561@bvrithyderabad.edu.in>
2022-12-06 07:33:21 +0530

    implementing closure

--- Level3/index.js
+++ Level3/index.js
@@ -1,7 +1,35 @@
-console.log('Hello 1')
+function generateGreetings (name) {
+  function spanish () {
+    console.log(`Hola ${name}!`)
+  }
+  function english () {
+    console.log(`Hello ${name}!`)
+  }
+  return { spanish, english }
+};
+const name = 'John'
+const greetings = generateGreetings(name)
 
-// Print message after 100 millisecond
-setTimeout(() => {
-  console.log('Hello 2')
-}, 500)
-console.log('Hello 3')
+console.log(typeof (greetings.spanish))
+
+greetings.spanish()
+greetings.english()
+
+// const firstName = () => console.log('John')
+// const lastName = () => console.log('Doe')
+
+// const printName = () => {
+//     console.log('My Name is:')
+//     setTimeout(firstName, 500)
+//     lastName()
+// }
+
+// printName()
+
+// console.log('Hello 1')
+
+// // Print message after 100 millisecond
+// setTimeout(() => {
+//   console.log('Hello 2')
+// }, 500)
+// console.log('Hello 3')
