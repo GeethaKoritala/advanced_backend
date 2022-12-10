649a06a4dd217383cfb208074572141f2cef6a84
20wh1a0561 <20wh1a0561@bvrithyderabad.edu.in>
2022-12-10 08:37:29 +0530

    using async and await

--- Level3/render.js
+++ Level3/render.js
@@ -1,38 +1,61 @@
-const fetchUserDetails = (userID) => {
+const time = async (ms) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve()
+    }, ms)
+  }) 
+}
+
+const fetchUserDetails = async(userID) => {
   console.log('fetching user details')
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('http://image.example.com/$(userID)')
-    }, 500)
-  }) 
+  await time(500)
+  return 'http://image.example.com/$(userID)'
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve('http://image.example.com/$(userID)')
+//     }, 500)
+//   }) 
 }
 
-const downloadImage = (imageURL, callback) => {
+const downloadImage = async (imageURL) => {
   console.log('Download Image')
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('image data for $(imageURL)')
-    }, 500)
-  }) 
+  await time(1000)
+  return 'image data for $(imageURL)'
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve('image data for $(imageURL)')
+//     }, 500)
+//   }) 
 }
 
-const render = (image) => {
+const render = async (image) => {
+  await time(1000)
   console.log('Render Image')
 }
-fetchUserDetails('john')
-  .then((imageURL) => downloadImage(imageURL))
-  .then((ImageData) => render(ImageData))
-  .catch((error) => {
-    console.log(error)
-  })
+
+const run = async () => {
+  const imageURL = await fetchUserDetails('john')
+  const imageData = await downloadImage(imageURL)
+  await render(imageData)
+}
+run()
+
+
+// fetchUserDetails('john')
+//   .then((imageURL) => downloadImage(imageURL))
+//   .then((ImageData) => render(ImageData))
+//   .catch((error) => {
+//     console.log(error)
+//   })
 // fetchUserDetails('john', (imageURL) => {
 //   downloadImage(imageURL, (imageData) => {
 //     render(imageData)
 //   })
 // })
 
+// PROMISE SYNTAX
 // const aPromise = new Promise((resolve, reject) => {
-
 // })
+// ASYNC TO MAKE PROMISE EASIER
 
 
