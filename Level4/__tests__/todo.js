6b8c0b0b0ab019de293cb77dbc5ff2ece6d3fb35
20wh1a0561 <20wh1a0561@bvrithyderabad.edu.in>
2022-12-17 18:23:18 +0530

    installing husky

--- Level4/__tests__/todo.js
+++ Level4/__tests__/todo.js
@@ -1,20 +1,27 @@
-const todoList = require('../todo');
+const todoList = require("../todo");
 
 const { all, markAsComplete, add } = todoList();
 
-describe("TodoList Test Suite",() => {
-    test("Should add new todo",() => {
-        expect(all.length).toBe(0);
-        add({
-            title : "Test todo",
-            completed : false,
-            dueDate : new Date().toLocaleDateString("en-CA")
-        });
-        expect(all.length).toBe(1);
+describe("TodoList Test Suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
     });
-    test("Should mark a todo as complete",() => {
-        expect(all[0].completed).toBe(false);
-        markAsComplete(0);
-        expect(all[0].completed).toBe(true);
+  });
+  test("Should add new todo", () => {
+    const todoItemsCount = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
     });
+    expect(all.length).toBe(todoItemsCount + 1);
+  });
+  test("Should mark a todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
 });
